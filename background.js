32e2cd1824c6ab0ea28554c8937e374753d697fc
MGCounts <68518563+mgcountss@users.noreply.github.com>
2022-07-23 19:02:38 -0500

    did a thing
    still gotta update the progress to next level thing

--- background.js
+++ background.js
@@ -1,14 +1,4 @@
-// // background.js
-// console.log('background loaded!')
-
-// chrome.runtime.onInstalled.addListener(function(details) {
-//     chrome.storage.sync.set({reset_timer: true});
-//     console.log('reset_timer stored as true')
-// });
-
-
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-	// console.log('(background.js) message recieved: '+request.message);
 	if (request.message === "reload") {
 		chrome.tabs.getSelected(null, function (tab) {
 			var code = 'window.location.reload(true);';
@@ -16,47 +6,20 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 		});
 	}
 
-
 	if (request.message === "hi") {
 		chrome.tabs.getSelected(null, function (tab) {
 			var code = `
-    fetch('https://mee6.xyz/api/plugins/levels/leaderboard/'+document.URL.split('/leaderboard/')[1]+'')
-		  .then(response => {
+			fetch('https://mee6.xyz/api/plugins/levels/leaderboard/' + document.URL.split('/leaderboard/')[1] + '')
+			.then(response => {
 			  return response.json()
-		  })
-		  .then(data => {
+			})
+			.then(data => {
 			  for (let q = 1; q < 100; q++) {
-				let newHTML1 = '<span class="tooltiptext">' + addCommas(data.players[q - 1].message_count) + '</span>'
-				let newHTML2 = '<span class="tooltiptext">' + addCommas(data.players[q - 1].xp) + '</span>'
-				document.querySelector("#app-mount > div > div > div.leaderboardBody > div.leaderboardPlayersListContainer > div > div > div:nth-child(" + q + ") > div.leaderboardPlayer > div.leaderboardPlayerStats > div:nth-child(1)").innerHTML += newHTML1
-				document.querySelector("#app-mount > div > div > div.leaderboardBody > div.leaderboardPlayersListContainer > div > div > div:nth-child(" + q + ") > div.leaderboardPlayer > div.leaderboardPlayerStats > div:nth-child(2)").innerHTML += newHTML2
-				document.querySelector("#app-mount > div > div > div.leaderboardBody > div.leaderboardPlayersListContainer > div > div > div:nth-child(" + q + ") > div.leaderboardPlayer > div.leaderboardPlayerStats > div:nth-child(1) > div.leaderboardPlayerStatValue").innerHTML = abbreviateNumber(data.players[q - 1].message_count)
-				document.querySelector("#app-mount > div > div > div.leaderboardBody > div.leaderboardPlayersListContainer > div > div > div:nth-child(" + q + ") > div.leaderboardPlayer > div.leaderboardPlayerStats > div:nth-child(2) > div.leaderboardPlayerStatValue").innerHTML = abbreviateNumber(data.players[q - 1].xp)
-				document.querySelector("#app-mount > div > div > div.leaderboardBody > div.leaderboardPlayersListContainer > div > div > div:nth-child(" + q + ") > div.leaderboardPlayer > div.leaderboardPlayerStats > div.leaderboardPlayerStat").classList = "leaderboardPlayerStat p" + Math.floor(progress(data.players[q - 1].detailed_xp[0], data.players[q - 1].detailed_xp[1])) + ""
-				}
-		  })
-		  function addCommas(count) {
-			return count.toLocaleString("en-US");
-		   }
-		   function progress(partialValue, totalValue) {
-			return (100 * partialValue) / totalValue;
-		 } 
-		 function abbreviateNumber(value) {
-			if (value < 1000) {
-		  return value
-			} else {
-			let newValue = value;
-			const suffixes = ["", "K", "M", "B", "T"];
-			let suffixNum = 0;
-			while (newValue >= 1000) {
-			  newValue /= 1000;
-			  suffixNum++;
-			}
-			newValue = newValue.toFixed(1);
-			newValue += suffixes[suffixNum];
-			return newValue;
-			}
-		  }
+				document.querySelector("#app-mount > div > div > div.w-full.max-w-\\[1440px\\].flex.flex-col-reverse.lg\\:flex-row.lg\\:p-8 > div.flex.grow.rounded-sm.lg\\:bg-legacy-card.shadow > div > div > div:nth-child(" + q + ") > div.flex.h-14.items-center.group.flex-col.py-8.sm\\:flex-row > div.hidden.sm\\:flex.items-center.self-center.ml-auto.text-legacy-faded > div:nth-child(1) > div.font-bold.text-lg.mt-1.text-white").innerHTML = data.players[q - 1].message_count.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+				document.querySelector("#app-mount > div > div > div.w-full.max-w-\\[1440px\\].flex.flex-col-reverse.lg\\:flex-row.lg\\:p-8 > div.flex.grow.rounded-sm.lg\\:bg-legacy-card.shadow > div > div > div:nth-child(" + q + ") > div.flex.h-14.items-center.group.flex-col.py-8.sm\\:flex-row > div.hidden.sm\\:flex.items-center.self-center.ml-auto.text-legacy-faded > div:nth-child(2) > div.font-bold.text-lg.mt-1.text-white").innerHTML = data.players[q - 1].xp.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+			  }
+			})
+		}
 		  `;
 			chrome.tabs.executeScript(tab.id, { code: code });
 		});
@@ -65,43 +28,17 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 	if (request.message === "start") {
 		chrome.tabs.getSelected(null, function (tab) {
 			var code = `
-    fetch('https://mee6.xyz/api/plugins/levels/leaderboard/'+document.URL.split('/leaderboard/')[1]+'')
-				.then(response => {
-					return response.json()
-				})
-				.then(data => {
-					for (let q = 1; q < 100; q++) {
-						let newHTML1 = '<span class="tooltiptext">' + addCommas(data.players[q - 1].message_count) + '</span>'
-						let newHTML2 = '<span class="tooltiptext">' + addCommas(data.players[q - 1].xp) + '</span>'
-						document.querySelector("#app-mount > div > div > div.leaderboardBody > div.leaderboardPlayersListContainer > div > div > div:nth-child(" + q + ") > div.leaderboardPlayer > div.leaderboardPlayerStats > div:nth-child(1)").innerHTML += newHTML1
-						document.querySelector("#app-mount > div > div > div.leaderboardBody > div.leaderboardPlayersListContainer > div > div > div:nth-child(" + q + ") > div.leaderboardPlayer > div.leaderboardPlayerStats > div:nth-child(2)").innerHTML += newHTML2
-						document.querySelector("#app-mount > div > div > div.leaderboardBody > div.leaderboardPlayersListContainer > div > div > div:nth-child(" + q + ") > div.leaderboardPlayer > div.leaderboardPlayerStats > div:nth-child(1) > div.leaderboardPlayerStatValue").innerHTML = abbreviateNumber(data.players[q - 1].message_count)
-						document.querySelector("#app-mount > div > div > div.leaderboardBody > div.leaderboardPlayersListContainer > div > div > div:nth-child(" + q + ") > div.leaderboardPlayer > div.leaderboardPlayerStats > div:nth-child(2) > div.leaderboardPlayerStatValue").innerHTML = abbreviateNumber(data.players[q - 1].xp)
-						document.querySelector("#app-mount > div > div > div.leaderboardBody > div.leaderboardPlayersListContainer > div > div > div:nth-child(" + q + ") > div.leaderboardPlayer > div.leaderboardPlayerStats > div.leaderboardPlayerStat").classList = "leaderboardPlayerStat p" + Math.floor(progress(data.players[q - 1].detailed_xp[0], data.players[q - 1].detailed_xp[1])) + ""
-					}
-				})
-				function abbreviateNumber(value) {
-					if (value < 1000) {
-				  return value
-					} else {
-					let newValue = value;
-					const suffixes = ["", "K", "M", "B", "T"];
-					let suffixNum = 0;
-					while (newValue >= 1000) {
-					  newValue /= 1000;
-					  suffixNum++;
-					}
-					newValue = newValue.toFixed(1);
-					newValue += suffixes[suffixNum];
-					return newValue;
-					}
-				  }
-				function addCommas(count) {
-				  return count.toLocaleString("en-US");
-				 }
-				 function progress(partialValue, totalValue) {
-					return (100 * partialValue) / totalValue;
-				 } 
+			fetch('https://mee6.xyz/api/plugins/levels/leaderboard/' + document.URL.split('/leaderboard/')[1] + '')
+			.then(response => {
+			  return response.json()
+			})
+			.then(data => {
+			  for (let q = 1; q < 100; q++) {
+				document.querySelector("#app-mount > div > div > div.w-full.max-w-\\[1440px\\].flex.flex-col-reverse.lg\\:flex-row.lg\\:p-8 > div.flex.grow.rounded-sm.lg\\:bg-legacy-card.shadow > div > div > div:nth-child(" + q + ") > div.flex.h-14.items-center.group.flex-col.py-8.sm\\:flex-row > div.hidden.sm\\:flex.items-center.self-center.ml-auto.text-legacy-faded > div:nth-child(1) > div.font-bold.text-lg.mt-1.text-white").innerHTML = data.players[q - 1].message_count.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+				document.querySelector("#app-mount > div > div > div.w-full.max-w-\\[1440px\\].flex.flex-col-reverse.lg\\:flex-row.lg\\:p-8 > div.flex.grow.rounded-sm.lg\\:bg-legacy-card.shadow > div > div > div:nth-child(" + q + ") > div.flex.h-14.items-center.group.flex-col.py-8.sm\\:flex-row > div.hidden.sm\\:flex.items-center.self-center.ml-auto.text-legacy-faded > div:nth-child(2) > div.font-bold.text-lg.mt-1.text-white").innerHTML = data.players[q - 1].xp.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+			  }
+			})
+		} 
 		  `;
 			chrome.tabs.executeScript(tab.id, { code: code });
 		});
