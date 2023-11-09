15578bf96ec239be0132f3308afb93e795f32ffa
MGCounts <68518563+mgcountss@users.noreply.github.com>
2023-11-08 23:03:22 -0600

    Add files via upload

--- background.js
+++ background.js
@@ -1,46 +1,45 @@
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 	if (request.message === "reload") {
-		chrome.tabs.getSelected(null, function (tab) {
-			var code = 'window.location.reload(true);';
-			chrome.tabs.executeScript(tab.id, { code: code });
+	  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+		const tab = tabs[0];
+		const code = 'window.location.reload(true);';
+		chrome.scripting.executeScript({
+		  target: { tabId: tab.id },
+		  function: (code) => {
+			eval(code);
+		  },
+		  args: [code],
 		});
+	  });
 	}
-
-	if (request.message === "hi") {
-		chrome.tabs.getSelected(null, function (tab) {
-			var code = `
-			fetch('https://mee6.xyz/api/plugins/levels/leaderboard/' + document.URL.split('/leaderboard/')[1] + '')
-			.then(response => {
-			  return response.json()
-			})
-			.then(data => {
-			  for (let q = 1; q < 100; q++) {
-				document.querySelector("#app-mount > div > div > div.w-full.max-w-\\[1440px\\].flex.flex-col-reverse.lg\\:flex-row.lg\\:p-8 > div.flex.grow.rounded-sm.lg\\:bg-legacy-card.shadow > div > div > div:nth-child(" + q + ") > div.flex.h-14.items-center.group.flex-col.py-8.sm\\:flex-row > div.hidden.sm\\:flex.items-center.self-center.ml-auto.text-legacy-faded > div:nth-child(1) > div.font-bold.text-lg.mt-1.text-white").innerHTML = data.players[q - 1].message_count.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
-				document.querySelector("#app-mount > div > div > div.w-full.max-w-\\[1440px\\].flex.flex-col-reverse.lg\\:flex-row.lg\\:p-8 > div.flex.grow.rounded-sm.lg\\:bg-legacy-card.shadow > div > div > div:nth-child(" + q + ") > div.flex.h-14.items-center.group.flex-col.py-8.sm\\:flex-row > div.hidden.sm\\:flex.items-center.self-center.ml-auto.text-legacy-faded > div:nth-child(2) > div.font-bold.text-lg.mt-1.text-white").innerHTML = data.players[q - 1].xp.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
-			  }
-			})
-		}
-		  `;
-			chrome.tabs.executeScript(tab.id, { code: code });
+  
+	if (request.message === "hi" || request.message === "start") {
+	  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+		const tab = tabs[0];
+		const code = `
+		fetch('https://mee6.xyz/api/plugins/levels/leaderboard/' + document.URL.split('/leaderboard/')[1] + '')
+		.then(response => {
+		  return response.json()
+		})
+		.then(data => {
+		  for (let q = 1; q < data.players.length; q++) {
+			document.querySelector("#root > div:nth-child(3) > div > div.flex.flex-col.w-full.relative > div.grid.grid-cols-1.lg\\:grid-cols-3.gap-2.lg\\:gap-10.z-1.relative > div.lg\\:col-span-2 > div > div.grid.grid-cols-1.gap-1\\.5 > div:nth-child(" + q + ") > div > div.grid.grid-cols-1.lg\\:grid-cols-3 > p:nth-child(1)").innerHTML = (data.players[q - 1].message_count).toLocaleString('en-US');
+			document.querySelector("#root > div:nth-child(3) > div > div.flex.flex-col.w-full.relative > div.grid.grid-cols-1.lg\\:grid-cols-3.gap-2.lg\\:gap-10.z-1.relative > div.lg\\:col-span-2 > div > div.grid.grid-cols-1.gap-1\\.5 > div:nth-child(" + q + ") > div > div.grid.grid-cols-1.lg\\:grid-cols-3 > p:nth-child(1)").style.fontSize = "0.8em";
+			document.querySelector("#root > div:nth-child(3) > div > div.flex.flex-col.w-full.relative > div.grid.grid-cols-1.lg\\:grid-cols-3.gap-2.lg\\:gap-10.z-1.relative > div.lg\\:col-span-2 > div > div.grid.grid-cols-1.gap-1\\.5 > div:nth-child(" + q + ") > div > div.grid.grid-cols-1.lg\\:grid-cols-3 > p:nth-child(2)").innerHTML = (data.players[q - 1].xp).toLocaleString('en-US');
+			document.querySelector("#root > div:nth-child(3) > div > div.flex.flex-col.w-full.relative > div.grid.grid-cols-1.lg\\:grid-cols-3.gap-2.lg\\:gap-10.z-1.relative > div.lg\\:col-span-2 > div > div.grid.grid-cols-1.gap-1\\.5 > div:nth-child(" + q + ") > div > div.grid.grid-cols-1.lg\\:grid-cols-3 > p:nth-child(2)").style.fontSize = "0.8em";
+		  }
+		}).catch(err => {
+		  console.log(err);
 		});
-	}
-
-	if (request.message === "start") {
-		chrome.tabs.getSelected(null, function (tab) {
-			var code = `
-			fetch('https://mee6.xyz/api/plugins/levels/leaderboard/' + document.URL.split('/leaderboard/')[1] + '')
-			.then(response => {
-			  return response.json()
-			})
-			.then(data => {
-			  for (let q = 1; q < 100; q++) {
-				document.querySelector("#app-mount > div > div > div.w-full.max-w-\\[1440px\\].flex.flex-col-reverse.lg\\:flex-row.lg\\:p-8 > div.flex.grow.rounded-sm.lg\\:bg-legacy-card.shadow > div > div > div:nth-child(" + q + ") > div.flex.h-14.items-center.group.flex-col.py-8.sm\\:flex-row > div.hidden.sm\\:flex.items-center.self-center.ml-auto.text-legacy-faded > div:nth-child(1) > div.font-bold.text-lg.mt-1.text-white").innerHTML = data.players[q - 1].message_count.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
-				document.querySelector("#app-mount > div > div > div.w-full.max-w-\\[1440px\\].flex.flex-col-reverse.lg\\:flex-row.lg\\:p-8 > div.flex.grow.rounded-sm.lg\\:bg-legacy-card.shadow > div > div > div:nth-child(" + q + ") > div.flex.h-14.items-center.group.flex-col.py-8.sm\\:flex-row > div.hidden.sm\\:flex.items-center.self-center.ml-auto.text-legacy-faded > div:nth-child(2) > div.font-bold.text-lg.mt-1.text-white").innerHTML = data.players[q - 1].xp.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
-			  }
-			})
-		} 
-		  `;
-			chrome.tabs.executeScript(tab.id, { code: code });
+		`;
+		chrome.scripting.executeScript({
+		  target: { tabId: tab.id },
+		  function: (code) => {
+			eval(code);
+		  },
+		  args: [code],
 		});
+	  });
 	}
-});
+  });
+  
--- content.js
+++ content.js
@@ -1,17 +1,14 @@
-function run() {
-  clearInterval(a)
-  fetch('https://mee6.xyz/api/plugins/levels/leaderboard/' + document.URL.split('/leaderboard/')[1] + '')
-    .then(response => {
-      return response.json()
-    })
-    .then(data => {
-      for (let q = 1; q < 100; q++) {
-        document.querySelector("#app-mount > div > div > div.w-full.max-w-\\[1440px\\].flex.flex-col-reverse.lg\\:flex-row.lg\\:p-8 > div.flex.grow.rounded-sm.lg\\:bg-legacy-card.shadow > div > div > div:nth-child(" + q + ") > div.flex.h-14.items-center.group.flex-col.py-8.sm\\:flex-row > div.hidden.sm\\:flex.items-center.self-center.ml-auto.text-legacy-faded > div:nth-child(1) > div.font-bold.text-lg.mt-1.text-white").innerHTML = data.players[q - 1].message_count.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
-        document.querySelector("#app-mount > div > div > div.w-full.max-w-\\[1440px\\].flex.flex-col-reverse.lg\\:flex-row.lg\\:p-8 > div.flex.grow.rounded-sm.lg\\:bg-legacy-card.shadow > div > div > div:nth-child(" + q + ") > div.flex.h-14.items-center.group.flex-col.py-8.sm\\:flex-row > div.hidden.sm\\:flex.items-center.self-center.ml-auto.text-legacy-faded > div:nth-child(2) > div.font-bold.text-lg.mt-1.text-white").innerHTML = data.players[q - 1].xp.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
-      }
-    })
-}
-
-let a = setInterval(() => {
-    run()
-}, 100);
+fetch('https://mee6.xyz/api/plugins/levels/leaderboard/' + document.URL.split('/leaderboard/')[1] + '')
+  .then(response => {
+    return response.json()
+  })
+  .then(data => {
+    for (let q = 1; q < data.players.length; q++) {
+      document.querySelector("#root > div:nth-child(3) > div > div.flex.flex-col.w-full.relative > div.grid.grid-cols-1.lg\\:grid-cols-3.gap-2.lg\\:gap-10.z-1.relative > div.lg\\:col-span-2 > div > div.grid.grid-cols-1.gap-1\\.5 > div:nth-child(" + q + ") > div > div.grid.grid-cols-1.lg\\:grid-cols-3 > p:nth-child(1)").innerHTML = (data.players[q - 1].message_count).toLocaleString('en-US');
+      document.querySelector("#root > div:nth-child(3) > div > div.flex.flex-col.w-full.relative > div.grid.grid-cols-1.lg\\:grid-cols-3.gap-2.lg\\:gap-10.z-1.relative > div.lg\\:col-span-2 > div > div.grid.grid-cols-1.gap-1\\.5 > div:nth-child(" + q + ") > div > div.grid.grid-cols-1.lg\\:grid-cols-3 > p:nth-child(1)").style.fontSize = "0.8em";
+      document.querySelector("#root > div:nth-child(3) > div > div.flex.flex-col.w-full.relative > div.grid.grid-cols-1.lg\\:grid-cols-3.gap-2.lg\\:gap-10.z-1.relative > div.lg\\:col-span-2 > div > div.grid.grid-cols-1.gap-1\\.5 > div:nth-child(" + q + ") > div > div.grid.grid-cols-1.lg\\:grid-cols-3 > p:nth-child(2)").innerHTML = (data.players[q - 1].xp).toLocaleString('en-US');
+      document.querySelector("#root > div:nth-child(3) > div > div.flex.flex-col.w-full.relative > div.grid.grid-cols-1.lg\\:grid-cols-3.gap-2.lg\\:gap-10.z-1.relative > div.lg\\:col-span-2 > div > div.grid.grid-cols-1.gap-1\\.5 > div:nth-child(" + q + ") > div > div.grid.grid-cols-1.lg\\:grid-cols-3 > p:nth-child(2)").style.fontSize = "0.8em";
+    }
+  }).catch(err => {
+    console.log(err);
+  });
